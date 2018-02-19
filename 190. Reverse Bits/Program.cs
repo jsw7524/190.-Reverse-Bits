7022ecb79d433bc3f3ae4450dabc8585fe9cdee6
jsw7524 <jsw7524@gmail.com>
2018-02-19 15:42:16 +0800

    bit operation

--- 190. Reverse Bits/Program.cs
+++ 190. Reverse Bits/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace _190.Reverse_Bits
 {
+
+    public class Solution
+    {
+        public uint reverseBits(uint n)
+        {
+            uint result = 0;
+            for (int i = 31; i >=0; i--)
+            {
+                result = result | (uint) ((n & (0x1<<i) ) >>i) << (31-i) ;
+            }
+            return result;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ 190. Reverse BitsTests/SolutionTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _190.Reverse_Bits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _190.Reverse_Bits.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void Test1()
+        {
+            Solution mySolution = new Solution();
+            uint result = mySolution.reverseBits(0x1);
+            Assert.AreEqual(result, (uint)0x80000000);
+        }
+
+        [TestMethod()]
+        public void Test2()
+        {
+            Solution mySolution = new Solution();
+            uint result = mySolution.reverseBits(0x2);
+            Assert.AreEqual(result, (uint)0x40000000);
+        }
+        [TestMethod()]
+        public void Test256()
+        {
+            Solution mySolution = new Solution();
+            uint result = mySolution.reverseBits(0xFF);
+            Assert.AreEqual(result, (uint)0xFF000000);
+        }
+        [TestMethod()]
+        public void Test43261596()
+        {
+            Solution mySolution = new Solution();
+            uint t = 43261596;
+            uint result = mySolution.reverseBits(t);
+            Assert.AreEqual(result, Convert.ToUInt32("00111001011110000010100101000000", 2));
+        }
+    }
+}
